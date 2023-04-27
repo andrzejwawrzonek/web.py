3c9e2e4c385487caad769c513e5791591ee15165
Andrzej <andrzej.wawrzonek@gmail.com>
2023-04-27 18:46:50 +0200

    final version of command line interface

--- todo app/CLI.py
+++ todo app/CLI.py
@@ -1,57 +1,57 @@
-import functions
-
-todos = []
-
+from Modules import functions
 
 while True:
     user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("What todo you want to add?: ") + "\n"
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+        todos = functions.get_todos()
 
-            todos.append(todo)
+        todos.append(todo + "\n")
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+        functions.write_todos(todos)
 
-            print("Ok. You added", todo.strip('\n'), ".")
-        case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+        print("Ok. You added", todo.strip('\n'), ".")
+    elif user_action.startswith('show'):
+        todos = functions.get_todos()
 
-            new_todos = []
+        new_todos = []
 
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
+        for item in todos:
+            new_item = item.strip('\n')
+            new_todos.append(new_item)
 
-            for index, todo in enumerate(new_todos):
-                row = f"{index + 1}.{todo}"
-                print(row)
-        case 'edit':
-            number = int(input("Type a number of todo to edit: "))
+        for index, todo in enumerate(new_todos):
+            row = f"{index + 1}.{todo}"
+            print(row)
+    elif user_action.startswith('edit'):
+        try:
+            todo_number = int(user_action[5:]) - 1
             new_todo = input("What todo you want to add instead?: ")
-            todos[number] = new_todo
+            todos = functions.get_todos()
+            todos[todo_number] = new_todo
+            functions.write_todos(todos)
             print("Ok, success. You added", new_todo)
-        case 'complete':
-            """completed_todo = input("Type a todo you have completed: ")
-            todos.remove(completed_todo)"""
-            number_of_completed_todo = int(input("Type a number of todo you have completed: "))
-            completed_todo = todos[number_of_completed_todo - 1]
-            todos.remove(completed_todo)
-            print("You've deleted", completed_todo)
-        case 'exit':
-            print("Bye!")
-            break
-        case _:
-            print("No demanded function in program.")
+        except IndexError:
+            print("To-do doesn't exist.")
+    elif user_action.startswith('complete'):
+        try:
+            completed_todo = int(user_action[9:]) - 1
+            todos = functions.get_todos()
+            completed_todo_name = todos[completed_todo]
+
+            todos.pop(completed_todo)
+
+            functions.write_todos(todos)
+            print("You've deleted", completed_todo_name)
+        except IndexError:
+            print("To-do doesn't exist.")
+    elif user_action.startswith('exit'):
+        print("Bye!")
+        break
+    else:
+        print("No demanded function in program.")
 
 
 
